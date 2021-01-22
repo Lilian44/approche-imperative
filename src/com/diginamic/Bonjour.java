81f7ac8c32d370a86a4a5dd25545e12e66ff97c5
Lilian <lilian.guepin@gmail.com>
2021-01-22 15:53:46 +0100

    v1

---
+++ src/com/diginamic/Bonjour.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.diginamic;
+
+/**
+ * @author lguep
+ *
+ */
+public class Bonjour {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		double a = 1.8;
+		
+		System.out.println(a);
+		
+	}
+
+}
---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,34 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte a = 1;
+		short b = 1200;
+		int c = 100000;
+		long d = 5000000000L;
+		
+		float e = 1.8F;
+		double f = 2.2D;
+		char g = 'w';
+		boolean h = false;
+		String j = "Lilian";
+
+		String randomString = "Voici le résultat d'un calcul:\n 1+5=6";
+		
+		System.out.println(randomString);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(j);
+		
+		
+
+	}
+
+}
